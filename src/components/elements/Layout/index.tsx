39a7fcd62597afea221ed321c27c648105e120e3
Arxeno <rmfbkw04@gmail.com>
2024-04-22 22:26:51 +0700

    foundation

---
+++ src/components/elements/Layout/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { LayoutProps } from "./interface";
+
+const Layout = ({ children }: LayoutProps) => {
+  return <>{children}</>; // TODO: for context
+};
+
+export default Layout;
