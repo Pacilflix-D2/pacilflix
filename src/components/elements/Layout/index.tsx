f4bae0b37c0c4bf1bacf0dec17698a45b4f5be4d
Arxeno <rmfbkw04@gmail.com>
2024-04-22 22:39:45 +0700

    prettier

--- src/components/elements/Layout/index.tsx
+++ src/components/elements/Layout/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { LayoutProps } from "./interface";
+import React from 'react'
+import { LayoutProps } from './interface'
 
 const Layout = ({ children }: LayoutProps) => {
-  return <>{children}</>; // TODO: for context
-};
+  return <>{children}</> // TODO: for context
+}
 
-export default Layout;
+export default Layout
