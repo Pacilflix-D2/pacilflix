deefcf578a23f0bdace30515dbda34002ef936c4
Arxeno <rmfbkw04@gmail.com>
2024-04-23 14:39:37 +0700

    style: prettier

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,13 +14,13 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       keyframes: {
-        "caret-blink": {
-          "0%,70%,100%": { opacity: "1" },
-          "20%,50%": { opacity: "0" },
+        'caret-blink': {
+          '0%,70%,100%': { opacity: '1' },
+          '20%,50%': { opacity: '0' },
         },
       },
       animation: {
-        "caret-blink": "caret-blink 1.25s ease-out infinite",
+        'caret-blink': 'caret-blink 1.25s ease-out infinite',
       },
     },
   },
